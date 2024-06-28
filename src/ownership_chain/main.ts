e1ed478b420e6eb30afa0a91d5c1510879acc525
dpla <dpla@freeverse.io>
2024-06-28 12:48:17 +0200

    scrap Transfers and insert Asset and Transfers to DB

--- src/ownership_chain/main.ts
+++ src/ownership_chain/main.ts
@@ -2,30 +2,61 @@ import {TypeormDatabase} from '@subsquid/typeorm-store'
 import {processor, CONTRACT_ADDRESS, Context} from './processor'
 import * as ERC721UniversalContract from '../abi/UniversalContract'
 import {getAccountKey20FromBaseUri} from './util'
-import { OwnershipContract, Transfer } from '../model'
+import { Asset, OwnershipContract, Transfer } from '../model'
+import { v4 as uuidv4 } from 'uuid'
 
-import {Multicall} from '../abi/multicall'
 
 processor.run(new TypeormDatabase({supportHotBlocks: true, stateSchema: 'ownership_chain_processor'}), async (ctx) => {
     let detectedEvents: DetectedEvents = getDetectedEvents(ctx)
     let rawOwnershipContracts: RawOwnershipContract[] = detectedEvents.ownershipContracts
     let rawTransfers: RawTransfer[] = detectedEvents.transfers
-    console.log('rawTransfers:', rawTransfers)
-    console.log('rawOwnershipContracts:', rawOwnershipContracts)
 
-    // TODO init tx
+    // TODO init tx ****************************
     if (rawOwnershipContracts.length > 0) {
         const ownershipContractsModelArray = createOwnershipContractsModel(rawOwnershipContracts)
         await ctx.store.insert(ownershipContractsModelArray)
     }
 
-    // if (rawTransfers.length > 0) {
-    //     const transfersModelArray = createTransfersModel(rawTransfers)
-    //     await ctx.store.insert(transfersModelArray) 
-    // }
+    if (rawTransfers.length > 0) {
+        // TODO improve in order to trigger only one query to DB
+        let assetsDbMap: Map<string, Asset> = new Map()        
+        for (let rawTransfer of rawTransfers) {
+            const assetDb = await ctx.store.findOneBy(Asset, 
+                {
+                    ownershipContract: new OwnershipContract({id: rawTransfer.ownershipContract}),
+                    tokenId: rawTransfer.tokenId
+                }
+            );
+            if (assetDb) {
+                const assetLogicalId: AssetLogicalId = {tokenId: rawTransfer.tokenId.toString().toLowerCase(), ownershipContract: rawTransfer.ownershipContract.toLowerCase()}
+                assetsDbMap.set(generateAssetLogicalIdKey(assetLogicalId), assetDb)
+            }
+        }
+        console.log(`assetsDbMap.size:`, assetsDbMap.size)
 
-    // TODO close tx
+        // Create assets
+        const assetsModelMap = createAssetsModelMap(rawTransfers, assetsDbMap)
+        console.log(`insert ${assetsModelMap.size} Assets:`, assetsModelMap)
+        await ctx.store.upsert([...assetsModelMap.values()]) 
+        
+        console.log(`insert ${rawTransfers.length} transfers:`, rawTransfers)
+        const transfersModelArray = createTransfersModel(rawTransfers, assetsModelMap)
+        await ctx.store.insert(transfersModelArray) 
+
+        // TODO update assetTransfers[]
+    }
+
+    // TODO close tx **********************
 })
+
+interface AssetLogicalId {
+    tokenId: string,
+    ownershipContract: string,
+}
+
+function generateAssetLogicalIdKey(assetLogicalId: AssetLogicalId): string {
+    return `${assetLogicalId.tokenId}:${assetLogicalId.ownershipContract}`;
+}
 
 interface DetectedEvents{
     transfers: RawTransfer[]
@@ -40,6 +71,7 @@ interface RawTransfer {
     timestamp: Date
     blockNumber: number
     txHash: string
+    ownershipContract: string
 }
 
 interface RawOwnershipContract {
@@ -59,28 +91,55 @@ function createOwnershipContractsModel(rawOwnershipContracts: RawOwnershipContra
     return ownershipContractModel
 }
 
-function createTransfersModel(rawTransfers: RawTransfer[]): Transfer[] {
+function createTransfersModel(rawTransfers: RawTransfer[], assetsModalMap: Map<string, Asset>): Transfer[] {
     let transfersModel: Transfer[] = []
-    // for (let rt of rawTransfers) {
-    //     transfersModel.push(new Transfer({
-    //         id: rt.id,
-    //         asset: new Asset({id: rt.id}),
-    //         from: rt.from,
-    //         to: rt.to,
-    //         timestamp: rt.timestamp,
-    //         blockNumber: rt.blockNumber,
-    //         txHash: rt.txHash,
-    //     }))
-    // }
+    for (let rt of rawTransfers) {
+        transfersModel.push(new Transfer({
+            id: rt.id,
+            asset: assetsModalMap.get(rt.id)!,
+            from: rt.from,
+            to: rt.to,
+            timestamp: rt.timestamp,
+            blockNumber: rt.blockNumber,
+            txHash: rt.txHash,
+        }))
+    }
     return transfersModel  
+}
+
+function createAssetsModelMap(rawTransfers: RawTransfer[], assetsDbMap: Map<string, Asset>): Map<string, Asset> {
+    let assetsModelMap: Map<string, Asset> = new Map()
+    let assetsToAddDbMap: Map<string, Asset> = new Map(assetsDbMap)
+    for (let rt of rawTransfers) {
+        // check if the asset already exists, or a new ID must be created
+        let newId: string | undefined = undefined
+        const assetLogicalId: AssetLogicalId = {tokenId: rt.tokenId.toString().toLowerCase(), ownershipContract: rt.ownershipContract.toLowerCase()}
+        const assetLogicalIdKey = generateAssetLogicalIdKey(assetLogicalId)
+        if (assetsToAddDbMap.has(assetLogicalIdKey)) {
+            newId = assetsToAddDbMap.get(assetLogicalIdKey)!.id
+        } else {
+            newId = uuidv4()
+        }
+        const newAsset = new Asset({
+            id: newId,
+            ownershipContract: new OwnershipContract({id: rt.ownershipContract}),
+            tokenId: rt.tokenId,
+            owner: rt.to,
+            transfers: [], // TODO add new transfer to all its transfers
+        })
+        assetsModelMap.set(rt.id, newAsset)        
+        assetsToAddDbMap.set(assetLogicalIdKey, newAsset) // add in oder to avoid duplicates in same batch of asset upserts
+    }
+    return assetsModelMap
 }
 
 function getDetectedEvents(ctx: Context ): DetectedEvents {    
     let transfers: RawTransfer[] = []
     let ownershipContractsToInsertInDb: RawOwnershipContract[] = []
 
-    /* get contractListFromDB **/
+    /* get contractListFromDB **/    
     let ownershipContractsToCheck: Set<string> = new Set();
+    ownershipContractsToCheck.add('0x52f5de321c9595e7b11f0d91d8c3e816c7d715bb') // TODO remove
     
     for (let block of ctx.blocks) {
         for (let log of block.logs) {
@@ -91,7 +150,7 @@ function getDetectedEvents(ctx: Context ): DetectedEvents {
             const logDecoded = ERC721UniversalContract.events.NewERC721Universal.decode(log)
             console.log(logDecoded)
                        
-            ownershipContractsToCheck.add(logDecoded.newContractAddress) // addresses to check transfers
+            ownershipContractsToCheck.add(logDecoded.newContractAddress.toLowerCase()) // addresses to check transfers
             const laosContractAddress = getAccountKey20FromBaseUri(logDecoded.baseURI)
             ownershipContractsToInsertInDb.push({
                 id: logDecoded.newContractAddress,
@@ -102,16 +161,30 @@ function getDetectedEvents(ctx: Context ): DetectedEvents {
           }
 
           // Transfer of an asset that belongs to a tracked contract?
-          if(ownershipContractsToCheck.has(log.address) && log.topics[0] === ERC721UniversalContract.events.Transfer.topic){
-            let {from, to, tokenId} = ERC721UniversalContract.events.Transfer.decode(log)
+        //   ownershipContractsToCheck.forEach((value) => {
+        //     console.log('ownershipContractsToCheck:',value);
+        //     });
+
+        //   if(ownershipContractsToCheck.has(log.address.toLowerCase())){
+        //     console.log('********************************************************************')
+        //     console.log('log.topics[0]:', log.topics[0]);
+
+        //   }
+          if(ownershipContractsToCheck.has(log.address.toLowerCase()) && log.topics[0] === ERC721UniversalContract.events.Transfer.topic){
+            console.log('********************************************************************')
+            const logDecoded = ERC721UniversalContract.events.Transfer.decode(log)
+            console.log('Transfer detected:', logDecoded);
+
+            let {from, to, tokenId} = logDecoded
             transfers.push({
-                id: log.id,
+                id: uuidv4(),
                 tokenId,
                 from,
                 to,
                 timestamp: new Date(block.header.timestamp),
                 blockNumber: block.header.height,
                 txHash: log.transactionHash,
+                ownershipContract: log.address,
             })
           }          
         }

b0bb04e1cb96c109f2ec201f8f7c98a5584164cc
Jürgen Kudla <kudlaj@gmail.com>
2024-07-01 11:37:12 +0200

    commands

--- db/migrations/1719825722668-Data.js
+++ db/migrations/1719825722668-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1719822203406 {
-    name = 'Data1719822203406'
+module.exports = class Data1719825722668 {
+    name = 'Data1719825722668'
 
     async up(db) {
         await db.query(`CREATE TABLE "transfer" ("id" character varying NOT NULL, "from" text NOT NULL, "to" text NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "block_number" integer NOT NULL, "tx_hash" text NOT NULL, "asset_id" character varying, CONSTRAINT "PK_fd9ddbdd49a17afcbe014401295" PRIMARY KEY ("id"))`)
